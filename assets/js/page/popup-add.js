0c97b738b1062111109e0ed5ff48621751c77692
emirontop3 <ozl.kpz@gmail.com>
2026-05-25 18:20:42 +0300

    ⚡ Automated sync: assets/js/page/popup-add.js

---
+++ assets/js/page/popup-add.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $('#save-btn-popup').click(function () {
+        if(document.getElementById("inputGroupFile04").value){
+			$(this).addClass("d-none");
+			$('.spiner').removeClass("d-none");
+		}
+		else {
+			//$('.spiner').removeClass("d-none");
+			alert ("Lütfen dosya yükleyiniz");
+		}
+    });
+
+});
